734f94f97238616485b343f95a309597c6d4c79f
Janusz Slota <janusz.slota@nixilla.com>
2013-10-09 10:53:40 +0100

    Insights fixes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nixilla_api_logger');
+        $treeBuilder->root('nixilla_api_logger');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
