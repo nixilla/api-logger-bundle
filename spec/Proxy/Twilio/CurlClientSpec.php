03651e204cd0b5fc39ab15183365ed472013804d
Janusz Slota <janusz.slota@nixilla.com>
2018-06-21 13:19:21 +0100

    adds missed test

--- spec/Proxy/Twilio/CurlClientSpec.php
+++ spec/Proxy/Twilio/CurlClientSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Nixilla\Api\LoggerBundle\Proxy\Twilio;
 
+use Nixilla\Api\LoggerBundle\Logger\ApiInterface;
 use Nixilla\Api\LoggerBundle\Proxy\Twilio\CurlClient;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -11,5 +12,24 @@ class CurlClientSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(CurlClient::class);
+    }
+
+    function it_monitors_http_requests(ApiInterface $logger)
+    {
+        $this->setLogger($logger);
+
+        $logger->logCall(
+            Argument::any(),
+            Argument::any(),
+            Argument::any(),
+            Argument::any(),
+            Argument::any(),
+            Argument::any(),
+            Argument::any(),
+            Argument::any()
+        )->shouldBeCalled()
+        ;
+
+        $this->request('GET', 'http://packagist.org', [], [], ['Content-Type' => 'text/plain'])->shouldNotReturn(null);
     }
 }
